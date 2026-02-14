23a23e26079d5b0afc32dfc3a33eba7fbf966208
Ervin Joshua C. Bosito <wicker3190@gmail.com>
2026-02-14 20:49:13 +0800

    new xmls

--- app/src/main/java/com/example/nutify/Login.kt
+++ app/src/main/java/com/example/nutify/Login.kt
@@ -9,21 +9,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        auth = FirebaseAuth.getInstance()
+
+
         val register = findViewById<TextView>(R.id.Redirect)
+        val home = findViewById<Button>(R.id.BTN_login)
+
+        home.setOnClickListener {
+            val i = Intent(this, Main::class.java)
+            startActivity(i)
+        }
 
         register.setOnClickListener {
             val i = Intent(this, Signup::class.java)
