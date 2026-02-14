991b423b72aeb5e508f92a47ffc7036437e94ab1
Ervin Joshua C. Bosito <wicker3190@gmail.com>
2026-02-14 20:49:55 +0800

    new xmls (working on signup kt function)

--- app/src/main/java/com/example/nutify/Signup.kt
+++ app/src/main/java/com/example/nutify/Signup.kt
@@ -2,27 +2,57 @@ package com.example.nutify
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.math.log
+import com.example.nutify.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Signup : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val login = findViewById<TextView>(R.id.Redirect)
+        val home = findViewById<TextView>(R.id.Redirect)
 
-        login.setOnClickListener {
+        binding.BTNsignup.setOnClickListener {
+            val name = binding.signupName.text.toString()
+            val email = binding.signupEmail.text.toString()
+            val password = binding.signupPassword.text.toString()
+            val conpassword = binding.signupPassword.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+                if (password. == conpassword) {
+
+                }
+            }
+
+
+        }
+
+        home.setOnClickListener {
             val i = Intent(this, Login::class.java)
             startActivity(i)
         }
